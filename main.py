7ce48545623daf609bc183858339f98ce9760657
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-23 12:06:46 -0700

    finished email quote of the day

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# import smtplib
-# import connect
+import smtplib
+import connect
 import datetime as dt
+import random
 
-# my_email = connect.account_email
-# password = connect.account_password
-#
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs="dave@dryweb.com",
-#                         msg="Subject: Hello\n\nThis is a test.")
-# connection.close()
+my_email = connect.account_email
+password = connect.account_password
 
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-print(day_of_week)
+date_to_send = dt.date(2023, 3, 23)
 
-date_of_birth = dt.datetime(year=2000, month=3, day=23)
-print(date_of_birth)
+with open("quotes.txt", "r") as quotes:
+    lines = quotes.readlines()
+
+daily_quote = random.choice(lines)
+
+
+# Check if today matches the day to send message
+if date_to_send == dt.date.today():
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="dave@dryweb.com",
+                            msg=f"Subject: Hello\n\n{daily_quote}")
+
+connection.close()
